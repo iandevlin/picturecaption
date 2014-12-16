e5f0b5bca04d80c27e1f343da22186e04d763a22
Ian Devlin <ian@iandevlin.com>
2014-12-16 19:29:19 +0100

    Attribute aria-labelledby on <img> is now set correctly when the image loads.

--- picturecaption.js
+++ picturecaption.js
@@ -14,7 +14,9 @@
             return src.replace(window.location.origin + window.location.pathname, '');
         },
         getDataById = function(labelId) {
+
             for (var i = 0; i < data.length; i++) {
+
                 if (data[i].labelledby === labelId) {
                     return data[i];
                 }
@@ -22,9 +24,12 @@
             return false;
         },
         checkFigcaption = function (el, isDefault) {
+
             if (el.hasAttribute('aria-labelledby')) {
+
                 labelledBy = el.getAttribute('aria-labelledby');
                 figcaption = document.getElementById(labelledBy);
+
                 if (figcaption) {
                     if (isDefault) {
                         defaultCaption = figcaption;
@@ -40,7 +45,9 @@
     
 
     for (var i = 0; i < figures.length; i++) {
+
         for (var j = 0; j < figures[i].children.length; j++) {
+
             var el = figures[i].children[j],
                 tag = el.tagName.toLowerCase(),
                 item,
@@ -49,22 +56,29 @@
             if (tag === 'picture') {
                 // store source elements and img element data
                 for (var k = 0; k < el.children.length; k++) {
+
                     var child = el.children[k],
                         childTag = child.tagName.toLowerCase(),
                         labelledBy = '',
                         caption = '',
                         figcaption;
+
                     if (childTag === 'source') {
                         // parse srcset
                         var srcs = child.srcset.split(',');
+
                         for (var m = 0; m < srcs.length; m++) {
                             var src = srcs[m].trim().split(' '),
                                 imgAlt = '';
+
                             imgSrc = parseSrc(src[0].trim());
+
                             if (child.hasAttribute('data-alt')) {
                                 imgAlt = child.getAttribute('data-alt');
                             }
+
                             checkFigcaption(child, false);
+
                             if (!getDataById(labelledBy)) {
                                 data.push({
                                     src: imgSrc,
@@ -83,19 +97,25 @@
                         }
                     }
                     else if (childTag === 'img') {
+
                         child.addEventListener('load', function(e) {
                             var data = lookup[parseSrc(e.target.currentSrc)];
                             defaultCaption.innerHTML = data.caption;
                             e.target.alt = data.alt;
+                            e.target.setAttribute('aria-labelledby', data.labelledby);
                         });
+
                         checkFigcaption(child, true);
+
                         imgSrc = parseSrc(child.src);
+
                         data.push({
                             labelledby: labelledBy,
                             src: imgSrc,
                             caption: caption,
                             alt: child.alt
                         });
+
                         lookup[imgSrc] = data[data.length - 1];
                     }
                 }
